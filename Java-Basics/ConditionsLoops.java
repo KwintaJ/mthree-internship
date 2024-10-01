a056f3ec80f4ae2ce443eff417648f7370a04ff7
Jan Kwinta <jankwinta@icloud.com>
2024-10-01 18:06:48 +0200

    functions for showing basic conditions instructions and loops

--- Java-Basics/ConditionsLoops.java
+++ Java-Basics/ConditionsLoops.java
@@ -1,26 +1,16 @@
 public class ConditionsLoops
 {
-    public static void main(String[] args)
-    {
-        int magicNumber = 8;
-
-        if(magicNumber == 0)
-        {
-            show();
-        }
-        else
-        {
-            show();
-        }
-
-        for (int i = 0; i < magicNumber; i++)
-        {
-            show();
-        }
-    }
+    public static int magicNumber = 8;
 
     public static void show()
     {
         System.out.println("hi");
     }
+
+    public static void main(String[] args)
+    {
+        
+    }
+
+    
 }
