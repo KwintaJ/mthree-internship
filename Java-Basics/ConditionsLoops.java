db2bb506dc0ff06d0149171f450d4cbc4aa2a00a
Jan Kwinta <jankwinta@icloud.com>
2024-10-01 17:54:10 +0200

    main funct

--- Java-Basics/ConditionsLoops.java
+++ Java-Basics/ConditionsLoops.java
@@ -1,3 +1,26 @@
-public class ConditionsLoops {
+public class ConditionsLoops
+{
+    public static void main(String[] args)
+    {
+        int magicNumber = 8;
 
-    public static void main(String[] args) {
+        if(magicNumber == 0)
+        {
+            show();
+        }
+        else
+        {
+            show();
+        }
+
+        for (int i = 0; i < magicNumber; i++)
+        {
+            show();
+        }
+    }
+
+    public static void show()
+    {
+        System.out.println("hi");
+    }
+}
