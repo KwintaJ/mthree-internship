71bdc21d7a2a29994a94bd57d85e9f042f2a2861
Jan Kwinta <jankwinta@icloud.com>
2024-10-02 13:08:00 +0200

    static properties

--- Java-Basics/ObjectsBasics.java
+++ Java-Basics/ObjectsBasics.java
@@ -1,11 +1,17 @@
 public class ObjectsBasics
 {
-    int num = 10;
+    static String constatS = "same"
+    int num = 8;
+    int varN;
 
     public static void main(String[] args)
     {
-        // ObjectsBasics obj1 = new ObjectsBasics();
-        // System.out.println(obj1.num);
+        System.out.println(new ObjectsBasics().num);
+
+        ObjectsBasics obj1 = new ObjectsBasics();
+        ObjectsBasics obj2 = new ObjectsBasics();
+
+
 
         System.out.println(new ObjectsBasics().num);
     }
