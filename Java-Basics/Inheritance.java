cecc39f5c2a5e82ac47717c983b8cac4099e2d36
Jan Kwinta <jankwinta@icloud.com>
2024-10-04 11:24:56 +0200

    method overloading

--- Java-Basics/Inheritance.java
+++ Java-Basics/Inheritance.java
@@ -1,18 +1,37 @@
+//super keyword
 class Parent
 {
     int num = 100;
+
+    Parent()
+    {
+        System.out.println("P");
+    }
+
+    String sayHi()
+    {
+        return "No";
+    }
 }
 
 class Child extends Parent
 {
     int num = 200;
+
+    @Override
+    String sayHi()
+    {
+        return "Hi";
+    }
 }
 
 public class Inheritance
 {
     public static void main(String[] args)
     {
-        Parent o = new Child();
+        //upcasting
+        Child o = new Child();
         System.out.println(o.num);
+        System.out.println(o.sayHi());
     }
 }
