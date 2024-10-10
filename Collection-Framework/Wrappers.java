98b85b56dc98c006f2357834c00dd01693e5d5f1
Jan Kwinta <jankwinta@icloud.com>
2024-10-10 13:58:09 +0200

    collections and their iterators

--- Collection-Framework/Wrappers.java
+++ Collection-Framework/Wrappers.java
@@ -2,6 +2,9 @@ public class Wrappers
 {
     public static void main(String[] args)
     {
-        
+        // Integer a = new Integer; // this is deprecated
+        Integer a = 10; // built-in class auto-boxing
+        int b = a; // auto-unboxing
+        System.out.println(a); // toString is already overriden        
     }
 }
