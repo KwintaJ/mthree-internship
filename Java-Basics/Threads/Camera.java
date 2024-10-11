1321a85ab4e033363d62d26539bfc8d31bad57f4
Jan Kwinta <jankwinta@icloud.com>
2024-10-11 14:16:48 +0200

    synchornized(this) block and passing objects to threads

--- Java-Basics/Threads/Camera.java
+++ Java-Basics/Threads/Camera.java
@@ -1,33 +1,58 @@
 class SmithFamily implements Runnable
 {
+    Camera c;
+
+    SmithFamily(Camera c)
+    {
+        this.c = c;
+    }
+
     @Override
     public void run()
     {
-        Camera.click();
+        c.click();
     }
 }
 
 class BrownFamily implements Runnable
 {
+    Camera c;
+
+    BrownFamily(Camera c)
+    {
+        this.c = c;
+    }
+
     @Override
     public void run()
     {
-        Camera.click();    
+        c.click();    
     }
 }
 
 public class Camera
 {
-    synchronized static void click()
+    /*synchronized static void click()
     {
         for(int i = 0; i < 7; i++)
             System.out.println(Thread.currentThread().getName() + " " + "clicks!");
+    }*/
+
+    void click()
+    {
+        synchronized(this)
+        {
+            for(int i = 0; i < 7; i++)
+                System.out.println(Thread.currentThread().getName() + " " + "clicks!");
+        }
     }
 
     public static void main(String[] args)
     {
-        SmithFamily s = new SmithFamily();
-        BrownFamily b = new BrownFamily();
+        Camera cam = new Camera();
+
+        SmithFamily s = new SmithFamily(cam);
+        BrownFamily b = new BrownFamily(cam);
 
         Thread t1 = new Thread(s);    
         Thread t2 = new Thread(b);    
