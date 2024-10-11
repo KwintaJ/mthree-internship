74bbab27566c69b473a44a9640bd1582debd7a37
Jan Kwinta <jankwinta@icloud.com>
2024-10-11 13:14:43 +0200

    multithreading, runnable interface vs thread class

---
+++ Java-Basics/Threading.java
@@ -0,0 +1,41 @@
+// Runnable vs Thread
+// Runnable - interface, Thread - class
+// Runnable - abstract run() and no start(), 
+// Thread - run(), start()
+
+class Task1 implements Runnable
+{
+    @Override
+    public void run()
+    {
+        for(int i = 0; i < 10; i++)
+            System.out.println("(1, " + i + ")");
+    }
+}
+
+class Task2 extends Thread
+{
+    @Override
+    public void run()
+    {
+        for(int i = 0; i < 10; i++)
+            System.out.println("(2, " + i + ")");
+    }
+}
+
+public class Threading
+{    
+    public static void main(String[] args)
+    {
+        Task1 taskA = new Task1();
+        Task2 taskB = new Task2();
+
+        Thread t1 = new Thread(taskA);
+        Thread t2 = new Thread(taskB);
+
+        // t1.run();
+        t1.start();
+        // t2.run();
+        t2.start();
+    }
+}
