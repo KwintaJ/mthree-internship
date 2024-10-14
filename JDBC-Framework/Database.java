401a223c0d500dcd0562c1f566ad06488b3d608c
Jan Kwinta <jankwinta@icloud.com>
2024-10-14 14:20:24 +0200

    JDBC! connection, query, update

---
+++ JDBC-Framework/Database.java
@@ -0,0 +1,110 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database
+{
+    private Connection connect = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+
+    public void readDataBase()
+    {
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/People?user=root&password=Jaispolka123");
+
+            statement = connect.createStatement();
+            resultSet = statement.executeQuery("select * from People.Users");
+
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while(resultSet.next())
+            {
+                for(int i = 1; i <= columnsNumber; i++)
+                {
+                    if(i > 1)
+                        System.out.print(", ");
+
+                    String columnValue = resultSet.getString(i);
+                    System.out.print(columnValue);
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
+        finally
+        {
+            try
+            {
+                connect.close();
+            }
+            catch(Exception e) {}
+        }
+    }
+
+    public void deleteUser(int id)
+    {
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/People?user=root&password=Jaispolka123");
+
+            statement = connect.createStatement();
+            statement.executeUpdate("delete from People.Users where ID = " + id + ";");
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
+        finally
+        {
+            try
+            {
+                connect.close();
+            }
+            catch(Exception e) {}
+        }
+    }
+
+    public void newUser(int id, String name)
+    {
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/People?user=root&password=Jaispolka123");
+
+            statement = connect.createStatement();
+            statement.executeUpdate("insert into People.Users(ID, Name) values ( " + id + ", \"" + name + "\");");
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
+        finally
+        {
+            try
+            {
+                connect.close();
+            }
+            catch(Exception e) {}
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Database db = new Database();
+        // db.deleteUser(6);
+        // db.newUser(6, "Tracey");
+        db.readDataBase();
+    }
+}
