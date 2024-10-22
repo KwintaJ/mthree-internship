6d6a6b20e2223e50d9bb076d367390d9d49edd5d
Jan Kwinta <jankwinta@icloud.com>
2024-10-22 10:34:27 +0200

    natural join, better code clarity

--- MySQL/MySQL-Joins.sql
+++ MySQL/MySQL-Joins.sql
@@ -13,50 +13,20 @@ CREATE TABLE IF NOT EXISTS StudentMarks (
 );
 
 -- INSERT INTO Subjects(SubjectName) values
---     ("Algebra"),
---     ("Calculus 101"),
---     ("English C1"),
---     ("Philosophy"),
---     ("Quantum Mechanics"),
---     ("Networking"),
---     ("Descrete Mathematics"),
---     ("Statistics and Probability Theory"),
+--     ("Algebra"), ("Calculus 101"), ("English C1"),
+--     ("Philosophy"), ("Quantum Mechanics"), ("Networking"), 
+--     ("Descrete Mathematics"), ("Statistics and Probability Theory"), 
 --     ("Digital Electronics");
 
 -- INSERT INTO StudentMarks(ID, SubjectID, Mark) values
---     (1, 1, 70),
---     (1, 3, 72),
---     (1, 4, 65),
---     (1, 8, 100),
---     (1, 9, 93),
---     (2, 2, 71),
---     (2, 3, 70),
---     (2, 5, 33),
---     (2, 6, 50),
---     (2, 7, 87),
---     (3, 1, 84),
---     (3, 2, 84),
---     (3, 4, 85),
---     (3, 6, 13),
---     (3, 7, 100),
---     (3, 9, 98),
---     (4, 5, 99),
---     (4, 6, 94),
---     (4, 7, 96),
---     (4, 8, 100),
---     (4, 9, 100),
---     (5, 1, 0),
---     (5, 3, 54),
---     (5, 5, 67),
---     (5, 6, 69),
---     (5, 8, 71),
---     (6, 1, 70),
---     (6, 2, 85),
---     (6, 5, 88),
---     (6, 9, 90),
---     (7, 3, 100);
+--     (1, 1, 70), (1, 3, 72), (1, 4, 65), (1, 8, 100), (1, 9, 93),
+--     (2, 2, 71), (2, 3, 70), (2, 5, 33), (2, 6, 50), (2, 7, 87),
+--     (3, 1, 84), (3, 2, 84), (3, 4, 85), (3, 6, 13), (3, 7, 100), (3, 9, 98),
+--     (4, 5, 99), (4, 6, 94), (4, 7, 96), (4, 8, 100), (4, 9, 100),
+--     (5, 1, 0), (5, 3, 54), (5, 5, 67), (5, 6, 69), (5, 8, 71),
+--     (6, 1, 70), (6, 2, 85), (6, 5, 88), (6, 9, 90), (7, 3, 100);
 
--- Simple join on
+-- Simple join on, natural join
 SELECT StudentInfo.ID as "StudentID", 
     Name as "StudentName", 
     SubjectName, 
@@ -64,8 +34,7 @@ SELECT StudentInfo.ID as "StudentID",
         FROM StudentInfo 
         JOIN StudentMarks 
         ON StudentInfo.ID = StudentMarks.ID
-        JOIN Subjects
-        ON Subjects.SubjectID = StudentMarks.SubjectID;
+        NATURAL JOIN Subjects;
 
 -- Average per subject
 SELECT SubjectName, AVG(Mark) as "AverageMark"
