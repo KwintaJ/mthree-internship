ae2fc58b0ee8b58e51d223f60a57b6459ba85fbe
Jan Kwinta <jankwinta@icloud.com>
2024-10-28 10:56:32 +0100

    book model

---
+++ Java-Advanced/Spring-Boot-REST/app/src/main/java/kwinta/app/Book.java
@@ -0,0 +1,15 @@
+package kwinta.app;
+
+public class Book
+{
+    int id;
+    String title;
+    String author;
+
+    Book(int id, String title, String author)
+    {
+        this.id = id
+        this.title = title
+        this.author = author
+    }
+}
